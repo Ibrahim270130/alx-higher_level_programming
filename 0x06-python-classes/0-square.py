8c9f7e99dd8ff67c2282f737f58948e65f3c6ef7
Ibrahim270130 <opajobiolakunle@gmail.com>
2022-12-20 15:57:39 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-
-
-
 class Square:
-    """Empty class Square that defines a square
-
-    Attributes:
-        empty
-        """
-        pass
+    pass
